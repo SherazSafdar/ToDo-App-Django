887a3e54c67f3d10054a70edcef91fb062cd60d6
Sheraz Safdar <sherazsafdar00@gmail.com>
2023-10-29 20:58:36 +0500

    Add Status

---
+++ todo_handler/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TaskCategory(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    STATUS_CHOICES = (
+        
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    category = models.ForeignKey('TaskCategory', on_delete=models.SET_NULL, null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='In Progress')
+    
+    def __str__(self):
+        return self.title    
+    
