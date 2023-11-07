70391e94c83c9a54f43df73f83a00135e73a36e0
Sheraz Safdar <sherazsafdar00@gmail.com>
2023-11-07 23:23:03 +0500

    add object it also in str

--- todo_handler/models.py
+++ todo_handler/models.py
@@ -1,30 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class TaskCategory(models.Model):
     name = models.CharField(max_length=100)
-
+    
     def __str__(self):
         return self.name
-
-class Priority(models.TextChoices):
-    LOW = 'Low', 'Low Priority'
-    MEDIUM = 'Medium', 'Medium Priority'
-    HIGH = 'High', 'High Priority'
-
-class Status(models.TextChoices):
-    TODO = 'To-Do', 'To-Do'
-    IN_PROGRESS = 'In Progress', 'In Progress'
-    COMPLETED = 'Completed', 'Completed'
-
+    
+STATUS_CHOICES = (
+    ('To-Do', 'To-Do'),
+    ('In Progress', 'In Progress'),
+    ('Completed', 'Completed'),)
+    
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     due_date = models.DateField(auto_now_add=True)
     category = models.ForeignKey(TaskCategory, on_delete=models.SET_NULL, null=True, blank=True)
-    priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.LOW)
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.TODO)
-
+    completed = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='To-Do')
+    
     def __str__(self):
-        return self.title
+        return f"title {self.title} id {self.id}"    
+    
+#
+#
